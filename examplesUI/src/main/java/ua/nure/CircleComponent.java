ae346e585e7a8f913265c86ade1eb2c338a680de
valentynliubchenko <lubchenko.val@gmail.com>
2026-04-29 09:52:54 +0300

    update circle

--- examplesUI/src/main/java/ua/nure/CircleComponent.java
+++ examplesUI/src/main/java/ua/nure/CircleComponent.java
@@ -2,9 +2,14 @@ package ua.nure;
 
 import javax.swing.*;
 import java.awt.*;
+import java.util.Random;
+import java.util.List;
 
 public class CircleComponent extends JComponent {
+    public static final int MAX_COORD = 100;
+
     private int circleNumber;
+    private static final List<Color> color = List.of(Color.BLUE, Color.RED, Color.GREEN, Color.YELLOW, Color.CYAN, Color.MAGENTA);
 
     public void setCircleNumber( int circleNumber ) {
         this.circleNumber = circleNumber;
@@ -13,8 +18,20 @@ public class CircleComponent extends JComponent {
 
     @Override
     public void paint(Graphics g) {
-        g.setColor( Color.BLUE );
-        g.drawOval( 0, 0, 50, 100 );
+        for ( int i = 0; i < circleNumber; i++ ) {
+            g.setColor( color.get( new Random().nextInt(color.size()) ) );
+            g.drawOval( randCoord(), randCoord(), randWidth(), randHeight() );
+        }
 
     }
+    private int randCoord(){
+        return new Random().nextInt(MAX_COORD);
+    }
+    private int randWidth(){
+        return new Random().nextInt(getWidth()/3);
+    }
+    private int randHeight(){
+        return new Random().nextInt(getHeight()/3);
+    }
+
 }
